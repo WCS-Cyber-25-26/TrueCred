1c03baf37c1f32a3790867b82468e844d30d2933
TejanveshG <gangavaraputejanvesh@gmail.com>
2026-01-09 17:53:39 -0500

    chore: setup canonicalization and hashing

---
+++ backend/services/canonical-service/app.js
@@ -0,0 +1,38 @@
+//Currently established as app.js, will be changed to server.js in Phase 2
+
+import { canonicalize, hashCanonicalString } from './canonicalize.js';
+
+function processStudentRecords() {
+    //add more student data if needed from payload.json
+    const studentRecords = [
+        {
+            university: "Western University",
+            studentName: "Alice Johnson",
+            degree: "Bachelor of Science",
+            degreeAwardedDate: "2024-06-12",
+            hiddenIdentifier: "X9F2K-8821",
+        },
+        {
+            university: "University of Toronto",
+            studentName: "Mohammed Ali",
+            degree: "Master of Engineering",
+            degreeAwardedDate: "2023-11-01",
+            hiddenIdentifier: "UTM-ENG-99341",
+        },
+    ];
+
+    for (const studentData of studentRecords) {
+    const canonicalString = canonicalize(studentData);
+    const hash = hashCanonicalString(canonicalString);
+
+    console.log("Canonical String:");
+    console.log(canonicalString);
+
+    console.log("SHA-256 Hash:");
+    console.log(hash);
+  }
+}
+
+processStudentRecords();
+
+//to run, execture node app.js in the canonical-service directory
---
+++ backend/services/canonical-service/canonicalize.js
@@ -0,0 +1,24 @@
+import crypto from "crypto";
+
+export function canonicalize(studentdata){
+  return "this function works";
+}
+
+export function hashCanonicalString(canonicalString){
+  return "this function works";
+}
+
+/*
+Test inputs provided in payload.json
+
+ input = {
+  university,
+  studentName,
+  degree,
+  degreeAwardedDate,
+  hiddenIdentifier
+ }
+ */
+
+ 
+
