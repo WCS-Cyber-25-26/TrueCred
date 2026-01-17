63c3e2128f6cc259efc0b1c2aa0a9a5a67256f86
KwakuAsa <145233021+KwakuAsa@users.noreply.github.com>
2026-01-16 22:22:52 -0500

    feat: implemented canonicalization and sha256 hashing

--- backend/services/canonical-service/canonicalize.js
+++ backend/services/canonical-service/canonicalize.js
@@ -1,12 +1,23 @@
 import crypto from "crypto";
 
-export function canonicalize(studentdata){
-  return "this function works";
+export function canonicalize(studentdata) {
+  let canonicalString = "";
+
+  canonicalString = Object.values(studentdata).map(value =>
+    String(value).trim().toLowerCase()).join("|")
+
+  return canonicalString;
+}
+//function for regular sha256 hashing
+function sha256(input) {
+  return crypto.createHash("sha256").update(input, "utf8").digest("hex");
 }
 
-export function hashCanonicalString(canonicalString){
-  return "this function works";
+export function hashCanonicalString(canonicalString) {
+  return sha256(canonicalString)
 }
+
+
 
 /*
 Test inputs provided in payload.json
@@ -20,5 +31,5 @@ Test inputs provided in payload.json
  }
  */
 
- 
+
 
