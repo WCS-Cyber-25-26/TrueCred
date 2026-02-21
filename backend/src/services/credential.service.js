6a7c8696712c0e7a9293329f10085530b7789de6
Uday Prashant <uprashan@uwo.ca>
2026-02-20 19:13:57 -0500

    feat: added getCredentialsByStudent for Get All Credentials for Student Endpoint #27

--- backend/src/services/credential.service.js
+++ backend/src/services/credential.service.js
@@ -1,13 +1,67 @@
 import prisma from '../../prisma/client.js';
 
 const credentialService = {
-    getCredentialsByStudent(){
+  async getCredentialsByStudent(studentId, userId) {
+    // Retrieving universityId from userId
+    const user = await prisma.university.findUnique({where: { userId: userId }, select: { id: true }});
 
-    },
+    if (!user) {
+      throw new Error("University not found for the given user ID.");
+    }
 
-    isCredentialRevoked(){
+    // Retrieval of credentials based on studentId and universityId
+    const credentials = await prisma.credential.findMany({
+      where: {
+        studentId: studentId,
+        universityId: user.id,
+      },
+      // select only the necessary fields
+      select: {
+        id: true,
+        universityId: true,
+        degreeName: true,
+        program: true,
+        canonicalHash: true,
+        awardedDate: true,
+        revocation: {
+          select: {
+            revokedAt: true,
+            reason: true,
+            revokedBy: true,
+          },
+        },
+      },
+      orderBy: {
+        awardedDate: 'desc',
+      },
+    });
 
-    },
-}
+    if (!credentials || credentials.length === 0) {
+      throw new Error("The student has no credentials from the authenticated university or the university does not match the credential issuer")
+    }
+
+    return credentials.map((cred) => {
+      const { revocation, ...rest } = cred;
+
+      // if no revocation record available, return status active
+      if (!revocation) {
+        return {
+          ...rest,
+          revokedAt: null,
+          reason: null,
+          revokedBy: null,
+          status: 'active',
+        };
+      }
+
+      // if revocation record exists, return status revoked
+      return {
+        ...rest,
+        ...revocation,
+        status: 'revoked',
+      };
+    });
+  },
+};
 
 export default credentialService;
